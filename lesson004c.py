4fd9165f30c4ba9cf7ea5054b732fa7250c55780
Mark Peters <mapeters101@earthlink.net>
2025-01-07 13:31:26 -0500

    Work on lesson004b from 3:05 to 3:30.

---
+++ lesson004c.py
@@ -0,0 +1,9 @@
+def sum(a, b, c):
+    return a + b + c
+
+num1 = int(input("Enter an integer: "))
+num2 = int(input("Enter an integer: "))
+num3 = int(input("Enter an integer: "))
+
+x = sum(num1, num2, num3)
+print(x)
