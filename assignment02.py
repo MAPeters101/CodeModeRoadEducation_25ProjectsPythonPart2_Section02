6e0a819ceef7e4dc42f840ca9bca5ef34b3e6f72
Mark Peters <mapeters101@earthlink.net>
2025-01-07 14:29:13 -0500

    Remove directive for assignment02.

--- assignment02.py
+++ assignment02.py
@@ -1,11 +1,3 @@
-# To check if a number is prime, we need a function. Let's call it isPrime.
-# This function should take a number as input and return True if it's a prime number, and False if it's not.
-
-#Prime number definiton
-# Any number less than or equal to 1 is not prime.
-# A prime number is only diviside by itself and 1
-
-#create your isPrime function. Make sure to include an appropriate function header. Remember, your function should return either True or False (no double quotes around it, just type True or False as is)
 def isPrime(num):
     if num <= 1:
         return False
@@ -19,12 +11,9 @@ def isPrime(num):
     return True
 
 
-#outside of the function, prompt the user to enter a number
 number = int(input("Please enter a number to check to see if it is a prime number: "))
 
-#call the function in the condition of an if statement. If the function returns True, then the if statement should run. The if statements states that the number is prime
 if isPrime(number):
     print(f"{number} is a prime number.")
-#else, print that the number is not prime
 else:
     print(f"{number} is not a prime number.")
