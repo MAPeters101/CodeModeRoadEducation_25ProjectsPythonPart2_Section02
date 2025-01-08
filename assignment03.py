08f6852912d6c991f86363be4d708943d8f71f9c
Mark Peters <mapeters101@earthlink.net>
2025-01-08 12:18:18 -0500

    Initial code for assignment03.

---
+++ assignment03.py
@@ -0,0 +1,34 @@
+print("Welcome to the Counting Game!\n")
+
+#initialize the count variable, setting it equal to 0
+
+
+# create a function to increase the count. Make sure to use the global keyword followed by the count variable name
+
+
+# create a function to reset the count. Make sure to use the global keyword followed by the count variable name
+
+# Main game loop
+
+
+  #first ask the user if they want to increase the count, reset the           count, or stop the count
+
+
+  #then use conditionals to call the appropriate function based on user's     input
+
+
+      # End the game if the player types 'stop' (HINT: just break)
+
+
+  #if the user didn't enter 'increase', 'reset', or 'stop', print that         their input was not valid (HINT: simply just use an else statement)
+
+
+  #at the end of each loop, print the current count value
+
+
+#outside of the loop, print the final count value
+
+
+
+print("Thanks for playing the game!")
+
