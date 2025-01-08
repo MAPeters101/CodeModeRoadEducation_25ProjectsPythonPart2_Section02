5cf8522b62dc1f2b3fd4528ec02f23f40a103124
Mark Peters <mapeters101@earthlink.net>
2025-01-08 15:02:49 -0500

    Get user input for assignment04.

--- assignment04.py
+++ assignment04.py
@@ -1,14 +1,15 @@
 # Give a header title for the recursive function that calculates the sum of all numbers in the range.
+def calculate_sum(start, stop):
 
+    # Base case: Check if start and stop are the same, return the value.
+    if start == stop:
+        return stop
+    # Replace this line with the return statement for the base case
 
-# Base case: Check if start and stop are the same, return the value.
-
-# Replace this line with the return statement for the base case
-
-# Recursive case: Add the current start to the sum of the rest.
-
-# Replace this line with the return statement for the recursive case
-
+    # Recursive case: Add the current start to the sum of the rest.
+    else:
+    # Replace this line with the return statement for the recursive case
+        return stop + calculate_sum(start, stop-1)
 
 # Give a header title for the recursive function that calculates the product of all numbers in the range.
 
@@ -26,7 +27,8 @@
 while True:
     try:
         # User input for start and stop numbers (stored into two variables)
-
+        start = int(input("Please enter the start value: "))
+        stop = int(input("Please enter the stop value: "))
         # Calculate (call the sum function) and print that value
 
         # Calculate (call the product function) and print the product
@@ -35,7 +37,8 @@ while True:
         choice = input("Do you want to continue? (YES/NO): ")
 
         # if choice is "NO" - check lowercase, then break out of the loop
-
+        if choice.lower() == "no":
+            break
 
     except ValueError:
         print("Please enter valid integers.")
