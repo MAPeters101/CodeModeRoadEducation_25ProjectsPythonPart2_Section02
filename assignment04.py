7e5e8b6a03fd3631514dd38b27650f08aed9e057
Mark Peters <mapeters101@earthlink.net>
2025-01-08 15:28:21 -0500

    Remove directive comments for assignment04.

--- assignment04.py
+++ assignment04.py
@@ -1,46 +1,24 @@
-# Give a header title for the recursive function that calculates the sum of all numbers in the range.
 def calculate_sum(start, stop):
-
-    # Base case: Check if start and stop are the same, return the value.
     if start == stop:
         return stop
-    # Replace this line with the return statement for the base case
-
-    # Recursive case: Add the current start to the sum of the rest.
     else:
-    # Replace this line with the return statement for the recursive case
         return start + calculate_sum(start+1, stop)
 
-# Give a header title for the recursive function that calculates the product of all numbers in the range.
 def calculate_product(start, stop):
-
-    # Base case: Check if start and stop are the same, return the value.
     if start == stop:
         return stop
-    # Replace this line with the return statement for the base case
-
-    # Recursive case: Multiply the current start with the product of the rest.
     else:
         return start * calculate_product(start+1, stop)
-    # Replace this line with the return statement for the recursive case
 
 
-# Main program
 while True:
     try:
-        # User input for start and stop numbers (stored into two variables)
         start = int(input("Please enter the start value: "))
         stop = int(input("Please enter the stop value: "))
-        # Calculate (call the sum function) and print that value
         print(f"The sum of the values between {start} and {stop} is {calculate_sum(start, stop)}.")
-
-        # Calculate (call the product function) and print the product
         print(f"The product of the values between {start} and {stop} is {calculate_product(start, stop)}.")
 
-        # Ask if the user wants to continue
         choice = input("Do you want to continue? (YES/NO): ")
-
-        # if choice is "NO" - check lowercase, then break out of the loop
         if choice.lower() == "no":
             break
 
